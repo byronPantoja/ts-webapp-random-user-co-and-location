828f82e44a4bf9923cbb3b7f2216e448a9ad73ae
“dbp” <byronpantoja@gmail.com>
2021-11-18 04:47:29 +0800

    adds Company class

--- src/User.ts
+++ src/User.ts
@@ -1,11 +1,18 @@
 import faker from "faker";
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      // faker type returns string, but we defined it as a number above. using parseFloat to return a number
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
